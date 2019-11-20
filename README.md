15de0baa035e07b7c1024e8cf40599db21438ad4
El-Chapo0133 <levequeloris66@gmail.com>
2019-11-20 06:55:00 +0100

    initial version, added class and how to use it simply {v0.0.1} && updated README

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # directoryControl_ps
 Simple class that control an AD directory
+
+## Parameters
+
+
+## Objectives
+	- Create a simple class that simplify AD directory management
+	- For an ETML's project prevention
+
+## Errors :(
+	- none for now :D
---
+++ main.ps1
@@ -0,0 +1,29 @@
+<#
+    * ******************************************************************************************************
+    * Author : Loris Levêque
+    * Date : 20.11.2019
+    * Description : Simple class that manage an AD directory
+    * ******************************************************************************************************
+    * Usage :
+    * + instantiation
+    *   $ $directoryControl = [DirectoryControl]::New()
+    *   
+    * + set Attribute
+    *   $ $directoryControl.Attribute = value
+    *   
+    * + call functions
+    *   $ $directoryControl.function(parameters)
+    *
+    * ******************************************************************************************************
+#>
+
+class DirectoryControl {
+    ## @Name addAttributeToUser()
+    ## @Desc Add an attribute to an ADUser
+    ## @Param 
+    ## 
+    ## @Return {nil}
+    [void] addAttributeToUser() {
+
+    }
+}
